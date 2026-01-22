734b8e59e2df6a509ce93b5953cfd661ad7bd050
Nhlamulo Mokwena <mokwenaphillemon9@gmail.com>
2026-01-22 13:29:40 +0200

    Project Initialization

---
+++ Bus-Finance-Frontend/frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+   <div className='text-blue-300'>
+    hello World
+   </div>
+  )
+}
+
+export default App
